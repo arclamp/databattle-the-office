bff0586ab5a6579c95cc0125d370b99580fe6239
Roni Choudhury <roni.choudhury@kitware.com>
2018-04-25 10:04:38 -0400

    Perform sentiment analysis

--- data/process.py
+++ data/process.py
@@ -5,6 +5,49 @@ import sys
 import unicodecsv as csv
 
 
+def sentiment_analysis(data):
+    from nltk.classify import NaiveBayesClassifier
+    from nltk.corpus import subjectivity
+    from nltk.sentiment import SentimentAnalyzer
+    from nltk.sentiment.util import *
+
+    n_instances = 100
+    subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories='subj')[:n_instances]]
+    obj_docs = [(sent, 'obj') for sent in subjectivity.sents(categories='obj')[:n_instances]]
+
+    train_subj_docs = subj_docs[:80]
+    test_subj_docs = subj_docs[80:100]
+    train_obj_docs = obj_docs[:80]
+    test_obj_docs = obj_docs[80:100]
+    training_docs = train_subj_docs+train_obj_docs
+    testing_docs = test_subj_docs+test_obj_docs
+
+    sentim_analyzer = SentimentAnalyzer()
+    all_words_neg = sentim_analyzer.all_words([mark_negation(doc) for doc in training_docs])
+
+    unigram_feats = sentim_analyzer.unigram_word_feats(all_words_neg, min_freq=4)
+    sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
+
+    training_set = sentim_analyzer.apply_features(training_docs)
+    test_set = sentim_analyzer.apply_features(testing_docs)
+
+    trainer = NaiveBayesClassifier.train
+    classifier = sentim_analyzer.train(trainer, training_set)
+
+    for key,value in sorted(sentim_analyzer.evaluate(test_set).items()):
+        print('{0}: {1}'.format(key, value))
+
+    from nltk.sentiment.vader import SentimentIntensityAnalyzer
+    from nltk import tokenize
+
+    sid = SentimentIntensityAnalyzer()
+    for line in data:
+        ss = sid.polarity_scores(line['line_text'])
+        line['compound'] = ss['compound']
+        line['neg'] = ss['neg']
+        line['pos'] = ss['pos']
+        line['neu'] = ss['neu']
+
 def count_phrase(phrase, data):
     speakers = {}
     for line in data:
@@ -45,6 +88,9 @@ def main():
         pprint.pprint(phrases[phrase])
         print
 
+    sentiment_analysis(data)
+
+    pprint.pprint(sorted(data, key=lambda x: x['pos'], reverse=True))
 
 if __name__ == '__main__':
     sys.exit(main())
